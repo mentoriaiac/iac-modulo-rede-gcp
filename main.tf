1e6fd122298a9a799fdde582a6375a7624951b67
Marcos Vinicius Rezende <marcos.rezende@ambevtech.com.br>
2021-09-13 21:09:24 -0300

    refactor: adicionando novos parametros de configuração.

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 locals {
-  firewall_name = format("%s-%s", var.project, var.vpc_name)
-}
+  firewall_name       = format("%s-%s", var.project, var.vpc_name)
+  _destination_ranges = var.direction == "INGRESS" ? var.destination_ranges : null
+  _source_ranges      = var.direction == "EGRESS" ? var.source_ranges : null
+  _source_tags        = var.direction == "EGRESS" ? var.source_tags : null
+  }
 
 resource "google_compute_network" "vpc_network" {
   project                 = var.project
@@ -18,21 +21,6 @@ resource "google_compute_subnetwork" "subnetworks" {
   network       = google_compute_network.vpc_network.id
 }
 
-resource "google_compute_firewall" "firewall" {
-  name    = local.firewall_name
-  project = var.project
-  network = google_compute_network.vpc_network.id
-
-  dynamic "allow" {
-    for_each = var.firewall_allow
-    content {
-      protocol = allow.value.protocol
-      ports    = allow.value.port
-    }
-  }
-
-}
-
 resource "google_compute_router" "default" {
   name    = "${var.vpc_name}-router"
   project = var.project
@@ -47,4 +35,25 @@ resource "google_compute_router_nat" "default" {
   router                             = google_compute_router.default.name
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
+
+resource "google_compute_firewall" "firewall_rules" {
+  name    = local.firewall_name
+  project = var.project
+  network = google_compute_network.vpc_network.id
+  direction          = var.direction
+  target_tags        = var.target_tags
+  source_ranges      = local._source_ranges
+  source_tags        = local._source_tags
+  destination_ranges = local._destination_ranges
+    
+  dynamic "allow" {
+    for_each = var.firewall_allow
+    content {
+      protocol = allow.value.protocol
+      ports    = allow.value.port
+    }
+  }
+
 }
--- versions.tf
+++ versions.tf
@@ -1,9 +1,10 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.0.0"
+  
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "3.73.0"
+      version = ">= 3.73.0"
     }
   }
-}
+  }
